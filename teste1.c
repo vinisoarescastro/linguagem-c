697d3d9ce9e2bb29bc2902d847d9db2c7edcc280
Vinicius Soares <vinisoarescastro@gmail.com>
2024-09-20 11:47:24 -0300

    list 1 ex 2

---
+++ teste1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Olá, tudo bem? Tenho 6 anos e programo.\n");
+
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor real: %f.\n", 3.141593);
+    printf("Valor real com apenas duas casas: %.2f.\n", 3.141593);
+    printf("Dado de texto: %c.\n", 'a');
+    printf("Dado de texto: %s.\n", "testando");
+    
+    return 0;
+
+}
