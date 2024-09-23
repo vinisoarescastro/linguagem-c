21c74fb5f2e79f7057882bb39dcbe75ffb8b3c08
Vinicius Soares <vinisoarescastro@gmail.com>
2024-09-23 15:00:35 -0300

    aula ling c

---
+++ aula 2.2 - scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 0;
+
+    printf("Valor inicial da sua idade: %i.\n", idade);
+
+    printf("Digite uma idade: \n");
+    scanf("%i", &idade);
+
+    printf("Idade informada: %i.\n", idade);
+}
